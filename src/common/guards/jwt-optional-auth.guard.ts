5480007e557e20e080f3ce6ee4f298b36652a20c
Dmytro Pavlov <dmitrypavlov99@ukr.net>
2025-01-28 12:50:05 +0400

    Revert "temporary remove everything jwt related"
    This reverts commit 9ab02487a83df44e7618e2e4c8cb60d621c11361.

---
+++ src/common/guards/jwt-optional-auth.guard.ts
@@ -0,0 +1,15 @@
+import { ExecutionContext, Injectable } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+
+@Injectable()
+export class JwtOptionalAuthGuard extends AuthGuard('jwt') {
+  canActivate(context: ExecutionContext) {
+    return super.canActivate(context);
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  handleRequest(err, user, info) {
+    // TODO: FIX OPTIONAL AUTH GUARD
+    return user;
+  }
+}
---
+++ src/features/user-platform/api/guards/local-auth.guard.ts
@@ -0,0 +1,39 @@
+import {
+  BadRequestException,
+  ExecutionContext,
+  HttpStatus,
+  Injectable,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { plainToClass } from 'class-transformer';
+import { validate } from 'class-validator';
+import { LoginUserDto } from '../input-dto/login-user.dto';
+
+@Injectable()
+export class LocalAuthGuard extends AuthGuard('local') {
+  async canActivate(context: ExecutionContext) {
+    const request = context.switchToHttp().getRequest<Request>();
+    // const response = context.switchToHttp().getResponse<Response>();
+
+    // transform the request object to class instance
+    const body = plainToClass(LoginUserDto, request.body);
+
+    // get a list of errors
+    const errors = await validate(body);
+
+    // extract error messages from the errors array
+    const errorsForResponse = [];
+    errors.forEach((error) => {
+      errorsForResponse.push({
+        field: error.property,
+        message: error.constraints[Object.keys(error.constraints)[0]],
+      });
+    });
+
+    if (errorsForResponse.length > 0) {
+      throw new BadRequestException(errorsForResponse);
+    }
+
+    return super.canActivate(context) as boolean | Promise<boolean>;
+  }
+}
